03104ff5efeaf81274dea0a54c468902c5229fc6
Danny <lorddarcness@hotmail.com>
2023-11-29 12:26:13 +0000

    CRUD controller/Routes added (idols)

--- controllers/idols.js
+++ controllers/idols.js
@@ -20,6 +20,77 @@ getAll = async (req, res) =>{
  res.status(200).json(idols);
 }
 
+create = async (req, res) =>{
+   const idols = {
+      stage_name: req.body.stage_name,
+      full_name: req.body.full_name,
+      korean_name: req.body.korean_name,
+      korean_stage_name: req.body.korean_stage_name,
+      dob: req.body.dob,
+      country: req.body.country,
+      birthplace: req.body.birthplace,
+      other_group: req.body.other_group,
+      gender: req.body.gender,
+      groupId: req.body.groupId
+   };
+   try{
+       if(idols.stage_name==null || idols.full_name==null || 
+         idols.korean_name==null || idols.korean_stage_name==null ||
+         idols.dob==null || idols.country==null || idols.birthplace==null ||
+         idols.gender==null || idols.groupId==null){
+               throw new Error("Essential fields missing");
+           }
+           await Idols.create(idols);
+           res.status(201).json(idols);
+   }
+   catch (error){
+       utilities.formatErrorResponse(res,400,error.message);
+   }
+}
+
+update = async (req, res) =>{
+   const id =req.body.id;
+   const idols = {
+      stage_name: req.body.stage_name,
+      full_name: req.body.full_name,
+      korean_name: req.body.korean_name,
+      korean_stage_name: req.body.korean_stage_name,
+      dob: req.body.dob,
+      country: req.body.country,
+      birthplace: req.body.birthplace,
+      other_group: req.body.other_group,
+      gender: req.body.gender,
+      groupId: req.body.groupId
+   };
+   try{
+       if(idols.stage_name==null || idols.full_name==null || 
+         idols.korean_name==null || idols.korean_stage_name==null ||
+         idols.dob==null || idols.country==null || idols.birthplace==null ||
+         idols.gender==null || idols.groupId==null){
+               throw new Error("Essential fields missing");
+           }
+           await Idols.update(idols,
+               {where: { id: id }}
+               );
+           res.status(200).json(idols);
+   }
+   catch (error){
+       utilities.formatErrorResponse(res,400,error.message);
+   }
+}
+deleting = async (req, res) =>{
+   const id =req.body.id;
+   try{
+       const deleted = await Idols.destroy({where: { id: id }});
+       if (deleted==0) {
+           throw new Error("Id not found");
+        }
+        res.status(200).send("Idol has been deleted");
+   }
+  catch(error){
+       utilities.formatErrorResponse(res,404,error.message);
+  }
+}
 
 
-module.exports = {getAll};
+module.exports = {getAll, deleting, update, create};
--- routes/idols.js
+++ routes/idols.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const upload = require('../middleware/upload');
 
 router.get('/', controller.getAll);
-
+router.post('/', controller.create);
+router.put('/', controller.update);
+router.delete('/', controller.deleting);
 
 module.exports = router;
