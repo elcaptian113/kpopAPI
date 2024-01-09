fa50831b3a9e69457b7d50984d7e102635ceb14b
Danny <lorddarcness@hotmail.com>
2024-01-09 11:02:53 +0000

    fixed fix

--- routes/idols.js
+++ routes/idols.js
@@ -197,7 +197,7 @@ router.get('/:id', controller.getById);
  *                  type: object
  *                  $ref: '#/definitions/Error'
  */
-router.get('/name/:value', controller.getByGroup);
+router.get('/group/:value', controller.getByGroup);
 
 /**
  * @swagger
